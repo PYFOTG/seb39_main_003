3ee9193bc4e32bdabdf61b8de95b5c5444dd49e4
mirea70 <deae670@daum.net>
2022-09-21 14:39:30 +0900

    통신7 시큐리티 해제 / corsOrigin 설정 변경

--- server/src/main/java/com/web/MyPetForApp/config/WebConfig.java
+++ server/src/main/java/com/web/MyPetForApp/config/WebConfig.java
@@ -14,7 +14,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로
 //                .allowedOrigins("http://localhost:3000") // 해당 origin 허용
-                .allowedOrigins("*")
+//                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedHeaders("*")
                 .allowedMethods("GET", "POST", "PATCH", "DELETE"); // 허용할 Request 메서드
     }
